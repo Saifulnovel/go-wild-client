9fe5b164549defc67a736387fe4b2ee8f0202125
Saifulnovel <sinovel07@gmail.com>
2022-11-07 23:08:10 +0600

    first commit

---
+++ src/component/Routes/Router/Routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import Home from '../../../Main/Home/Home';
+import Main from '../../../Main/Main';
+
+const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            
+            {
+                path: '/',
+                element:<Home></Home>
+            }
+        ]
+    }
+])
+export default routes;
